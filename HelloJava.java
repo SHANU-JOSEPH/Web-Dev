f264145e1e8eb5aa30a025ad17dfc1d473ce47f4
SHANU-JOSEPH <2021WB86406@wilp.bits-pilani.ac.in>
2025-05-04 23:49:43 +0530

    Updated commit with version v1.2

--- HelloJava.java
+++ HelloJava.java
@@ -1,6 +1,6 @@
 public class HelloJava{
 	public static void main(String []arrgs){
-		String version= "v1.1";
+		String version= "v1.2";
 		System.out.println("Hello Java version " + version );
 	}
 }
